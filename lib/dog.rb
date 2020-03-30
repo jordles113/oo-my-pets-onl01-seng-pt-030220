efaf17b2f18d89a9e493f4fbc321045e48268ec4
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-30 20:49:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,9 @@
 class Dog
-   attr_accessor :mood 
-  attr_reader :name, :owner 
-  
-  @@all = []
-  
-  def initialize(name, owner)
-    @name = name 
-    @owner = owner
-    @mood = "nervous"
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all
-  end 
+  attr_accessor :mood
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
