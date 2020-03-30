598fb3809d9bb5ad874f9e00af7fd1d4e7f7cc3e
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-30 20:31:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,15 @@
 class Owner
   attr_reader :name, :species  
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @species = "human"
+    @@all << self 
   end 
   
   def say_species()
-    puts "I am a #{species}."
+    puts "I am a #{@species}."
   end 
 end
