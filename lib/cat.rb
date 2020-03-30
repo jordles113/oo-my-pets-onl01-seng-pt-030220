699f9196374c5b9641b745258a1b1b4a79f8bdd5
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-30 20:39:41 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,9 @@
 class Cat
   attr_accessor :owner 
-  attr_reader :name 
+  attr_reader :name, :mood 
   def initialize(name, owner)
     @name = name 
     @owner = owner
+    @mood = "nervous"
   end 
 end
